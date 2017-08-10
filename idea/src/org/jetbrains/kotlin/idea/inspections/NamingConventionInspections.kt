6a32389c16f792c96b8f44391579e8cfc0b3b7df
Dmitry Jemerov <yole@jetbrains.com>
2017-08-10 18:53:46 +0200

    Naming convention inspections mostly done

--- idea/src/org/jetbrains/kotlin/idea/inspections/NamingConventionInspections.kt
+++ idea/src/org/jetbrains/kotlin/idea/inspections/NamingConventionInspections.kt
@@ -16,52 +16,142 @@
 
 package org.jetbrains.kotlin.idea.inspections
 
+import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.editor.event.DocumentAdapter
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.ui.LabeledComponent
+import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiNameIdentifierOwner
 import com.intellij.ui.EditorTextField
-import com.siyeh.ig.fixes.RenameFix
+import com.siyeh.ig.psiutils.TestUtils
 import org.intellij.lang.regexp.RegExpFileType
-import org.jetbrains.kotlin.psi.KtClassOrObject
-import org.jetbrains.kotlin.psi.KtVisitorVoid
+import org.jetbrains.kotlin.idea.quickfix.RenameIdentifierFix
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
+import org.jetbrains.kotlin.psi.psiUtil.visibilityModifierType
 import java.awt.BorderLayout
 import javax.swing.JPanel
 
-abstract class NamingConventionInspection : AbstractKotlinInspection() {
-    private var nameRegex: Regex = defaultNamePattern.toRegex()
+abstract class NamingConventionInspection(private val entityName: String,
+                                          private val defaultNamePattern: String) : AbstractKotlinInspection() {
+    protected var nameRegex: Regex = defaultNamePattern.toRegex()
     var namePattern: String = defaultNamePattern
         set(value) {
             field = value
             nameRegex = value.toRegex()
         }
 
-    abstract val defaultNamePattern: String
-
     protected fun verifyName(element: PsiNameIdentifierOwner, holder: ProblemsHolder) {
         val name = element.name
         val nameIdentifier = element.nameIdentifier
         if (name != null && nameIdentifier != null && !nameRegex.matches(name)) {
             holder.registerProblem(element.nameIdentifier!!,
-                                   "Class name <code>#ref</code> doesn''t match regex ''$namePattern'' #loc",
-                                   RenameFix())
+                                   "$entityName name <code>#ref</code> doesn't match regex '$namePattern' #loc",
+                                   RenameIdentifierFix())
         }
     }
 
     override fun createOptionsPanel() = NamingConventionOptionsPanel(this)
 }
 
-class ClassNameInspection : NamingConventionInspection() {
-    override val defaultNamePattern: String
-        get() = "[A-Z][A-Za-z\\d]*"
-
+class ClassNameInspection : NamingConventionInspection("Class", "[A-Z][A-Za-z\\d]*") {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : KtVisitorVoid() {
             override fun visitClassOrObject(classOrObject: KtClassOrObject) {
                 verifyName(classOrObject, holder)
             }
+
+            override fun visitEnumEntry(enumEntry: KtEnumEntry) {
+                // do nothing
+            }
+        }
+    }
+}
+
+class EnumEntryNameInspection : NamingConventionInspection("Enum entry", "[A-Z]([A-Za-z\\d]*|[A-Z_\\d]*)") {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return object : KtVisitorVoid() {
+            override fun visitEnumEntry(enumEntry: KtEnumEntry) {
+                verifyName(enumEntry, holder)
+            }
+        }
+    }
+}
+
+class FunctionNameInspection : NamingConventionInspection("Function", "[a-z][A-Za-z\\d]*") {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return object : KtVisitorVoid() {
+            override fun visitNamedFunction(function: KtNamedFunction) {
+                if (TestUtils.isInTestSourceContent(function) && function.nameIdentifier?.text?.startsWith("`") == true) {
+                    return
+                }
+                verifyName(function, holder)
+            }
+        }
+    }
+}
+
+abstract class PropertyNameInspectionBase protected constructor(private val kind: PropertyKind,
+                                                                entityName: String,
+                                                                defaultNamePattern: String)
+    : NamingConventionInspection(entityName, defaultNamePattern) {
+
+    protected enum class PropertyKind { NORMAL, PRIVATE, OBJECT, CONST, LOCAL }
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return object : KtVisitorVoid() {
+            override fun visitProperty(property: KtProperty) {
+                if (property.getKind() == kind) {
+                    verifyName(property, holder)
+                }
+            }
+        }
+    }
+
+    private fun KtProperty.getKind(): PropertyKind = when {
+        isLocal -> PropertyKind.LOCAL
+
+        containingClassOrObject is KtObjectDeclaration -> PropertyKind.OBJECT
+
+        hasModifier(KtTokens.CONST_KEYWORD) -> PropertyKind.CONST
+
+        visibilityModifierType() == KtTokens.PRIVATE_KEYWORD -> PropertyKind.PRIVATE
+
+        else -> PropertyKind.NORMAL
+    }
+}
+
+class PropertyNameInspection : PropertyNameInspectionBase(PropertyKind.NORMAL, "Property", "[a-z][A-Za-z\\d]*")
+
+class ObjectPropertyNameInspection : PropertyNameInspectionBase(PropertyKind.OBJECT, "Object property", "[A-Za-z][_A-Za-z\\d]*")
+
+class PrivatePropertyNameInspection : PropertyNameInspectionBase(PropertyKind.PRIVATE, "Private property", "_?[a-z][_A-Za-z\\d]*")
+
+class ConstPropertyNameInspection : PropertyNameInspectionBase(PropertyKind.CONST, "Const property", "[A-Z][_A-Z\\d]*")
+
+class LocalVariableNameInspection : PropertyNameInspectionBase(PropertyKind.LOCAL, "Local variable", "[a-z][A-Za-z\\d]*")
+
+class PackageNameInspection : NamingConventionInspection("Package", "[a-z][A-Za-z\\d\\.]*") {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean, session: LocalInspectionToolSession): PsiElementVisitor {
+        return object : KtVisitorVoid() {
+            override fun visitPackageDirective(directive: KtPackageDirective) {
+                if (!nameRegex.matches(directive.qualifiedName)) {
+                    holder.registerProblem(directive.packageNameExpression!!,
+                                           "Package name <code>#ref</code> doesn't match regex '$namePattern' #loc",
+                                           RenamePackageFix())
+                }
+            }
+        }
+    }
+
+    private class RenamePackageFix : RenameIdentifierFix() {
+        override fun getElementToRename(element: PsiElement): PsiElement? {
+            val packageDirective = element as? KtPackageDirective ?: return null
+            return JavaPsiFacade.getInstance(element.project).findPackage(packageDirective.qualifiedName)
         }
     }
 }
@@ -70,13 +160,15 @@ class NamingConventionOptionsPanel(owner: NamingConventionInspection) : JPanel()
     init {
         layout = BorderLayout()
 
-        val regexField = EditorTextField(owner.namePattern, null, RegExpFileType.INSTANCE)
+        val regexField = EditorTextField(owner.namePattern, null, RegExpFileType.INSTANCE).apply {
+            setOneLineMode(true)
+        }
         regexField.document.addDocumentListener(object : DocumentAdapter() {
             override fun documentChanged(e: DocumentEvent?) {
                 owner.namePattern = regexField.text
             }
         })
         val labeledComponent = LabeledComponent.create(regexField, "Pattern:", BorderLayout.WEST)
-        add(labeledComponent, BorderLayout.CENTER)
+        add(labeledComponent, BorderLayout.NORTH)
     }
 }
